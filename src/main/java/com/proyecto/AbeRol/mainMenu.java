7699e4e3fb5050a3c1ae284689038a6096a74d60
Miguel Angel Garcia Merida <garciameridamiguelangel@gmail.com>
2021-05-07 17:43:18 +0200

    update

--- src/main/java/com/proyecto/AbeRol/mainMenu.java
+++ src/main/java/com/proyecto/AbeRol/mainMenu.java
@@ -1,6 +1,8 @@
 package com.proyecto.AbeRol;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.proyecto.AbeRol.Model.MasterDAO;
 import com.proyecto.AbeRol.Model.Player;
@@ -8,58 +10,101 @@ import com.proyecto.AbeRol.Model.Rol;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class mainMenu  {
 	
 	@FXML
-	private Button deleteMasterButt;
+	private Button buttEditMaster;
 
 	@FXML
-	private Button deleteRolButt;
+	private Button buttEditRol;
 
 	@FXML
-	private Button addRolButt;
+	private Button buttEditPlayer;
 
 	@FXML
 	private Button buttExit;
 
 	@FXML
 	private ComboBox<Rol> choiceRol;
-	@FXML 
+	
+	
 	private ObservableList<Player> Players;
 	@FXML
-	private Label pitoDeLeche;
+	private Label P1Name;
 	
 	@FXML
 	public void initialize() {
 	 MasterDAO u = new MasterDAO();
-	//this.choiceRol.setItems((ObservableList<Rol>) u.getRol());
+	 u.getRol();
+	this.choiceRol.setItems((ObservableList<Rol>) u.getRol());
 	}
 
 	@FXML
-	protected void deleteMaster(ActionEvent Event) throws IOException {
+	protected void editMaster(ActionEvent Event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("editMaster.fxml"));
+		Parent modal;
+		try {
+			modal = fxmlLoader.load();
+			Stage modalStage = new Stage();
+			modalStage.initModality(Modality.APPLICATION_MODAL);
+			modalStage.initOwner(App.rootstage);
+			Scene modalScene = new Scene(modal);
+			modalStage.setScene(modalScene);
+			modalStage.showAndWait();
+			modalStage.setResizable(false);
 
+		} catch (IOException ex) {
+			Logger.getLogger(mainMenu.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 	@FXML
-	protected void deleteRol(ActionEvent Event) throws IOException {
+	protected void editRol(ActionEvent Event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("editRol.fxml"));
+		Parent modal;
+		try {
+			modal = fxmlLoader.load();
+			Stage modalStage = new Stage();
+			modalStage.initModality(Modality.APPLICATION_MODAL);
+			modalStage.initOwner(App.rootstage);
+			Scene modalScene = new Scene(modal);
+			modalStage.setScene(modalScene);
+			modalStage.showAndWait();
+			modalStage.setResizable(false);
 
+		} catch (IOException ex) {
+			Logger.getLogger(mainMenu.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 	@FXML
-	protected void addRol(ActionEvent Event) throws IOException {
+	protected void EditPlayer(ActionEvent Event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("editPlayer.fxml"));
+		Parent modal;
+		try {
+			modal = fxmlLoader.load();
+			Stage modalStage = new Stage();
+			modalStage.initModality(Modality.APPLICATION_MODAL);
+			modalStage.initOwner(App.rootstage);
+			Scene modalScene = new Scene(modal);
+			modalStage.setScene(modalScene);
+			modalStage.showAndWait();
+			modalStage.setResizable(false);
 
-	}
-	
-	@FXML 
-	protected void setColumn1() {
-		//this.pitoDeLeche..set(choiceRol.getSelectionModel().getSelectedItem());
+		} catch (IOException ex) {
+			Logger.getLogger(mainMenu.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 	
 	@FXML
@@ -68,12 +113,21 @@ public class mainMenu  {
         stage.close();
  
     }
+	
+	
+	
+	
+	@FXML 
+	protected void setColumn1() {
+		//this.pitoDeLeche..set(choiceRol.getSelectionModel().getSelectedItem());
+	}
+	
+	
 	public boolean showConfirm(String nombre) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirme la acción");
         alert.setHeaderText("¿Desea borrar el usuario " +nombre+ "?");
         alert.setContentText("Está a punto de eliminar su perfil, ¿desea hacerlo?");
-
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
             return true;
